bf4277e26472cbd43385a03a84f1165ded67b678
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-10-26 09:43:11 +0700

    Implement login system to login.html

---
+++ article/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title></title>
+</head>
+    <body style="background-color: #BFBCBC">
+        <div class="divh">
+            <div id="menuToggle">
+                <input type="checkbox"/>
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <a href="login.php">
+                        <li>Login</li>
+                    </a>
+                    <a href="#">
+                        <li>Register</li>
+                    </a>
+                </ul>
+            </div>
+            <p class="head"><a href="#">SS</a></p>
+        </div>
+        <div class="login">
+            <form method="POST">
+                <div class="imgcontainer">
+                    <img src="pic web/login.png" alt="Avatar" class="avatar">
+                </div>
+                <div class="container">
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required>
+
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required>
+
+                    <button type="submit">Login</button>
+
+                    <?php
+                    session_start();
+                    include('backend/user.php');
+                    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                        $user = new User();
+                        extract($_POST);
+                        $login = $user->check_login($username, $password);
+                        if ($login) {
+                            echo "logged in <br>";
+                            header('Location: /article');
+                        } else {
+                            echo "Login failed!<br>";
+                        }
+                    }
+                    ?>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
