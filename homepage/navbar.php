9636cdd2d63c08a905dac406981385f8320ab8f6
xzsawq47 <32704294+xzsawq47@users.noreply.github.com>
2018-11-03 15:12:02 +0700

    Add files via upload

---
+++ homepage/navbar.php
@@ -0,0 +1,43 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+?>
+<div class="divh">
+    <div id="menuToggle">
+        <input type="checkbox"/>
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+            <?php
+            if(!isset($_SESSION['login'])){
+
+                    echo '
+                    <a href="login.php">
+                        <li>Login</li>
+                    </a>
+                    <a href="register.php">
+                        <li>Register</li>
+                    </a>';
+                }
+                else if($_SESSION['login']){
+                    $username = $_SESSION['username'];
+                    echo '
+                    <a href="backend/profile.php?username='.$username.'">
+                        <li>'.$username.'</li>
+                    </a>
+                    <a href="backend/logout.php">
+                        <li>logout</li>
+                    </a>
+                    ';
+            }
+            ?>
+
+        </ul>
+    </div>
+    <p class="head"><a href="/article">SS</a></p>
+</div>
+
+
