6612afb7c9662c5140f8b72c9991dd140fdfbd55
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-11-03 14:20:23 +0700

    fix proflie.php

--- article/navbar.php
+++ article/navbar.php
@@ -25,7 +25,7 @@ if (session_status() == PHP_SESSION_NONE) {
                 else if($_SESSION['login']){
                     $username = $_SESSION['username'];
                     echo '
-                    <a href="backend/profile.php?username='.$username.'">
+                    <a href="profile.php?username='.$username.'">
                         <li>'.$username.'</li>
                     </a>
                     <a href="backend/logout.php">
